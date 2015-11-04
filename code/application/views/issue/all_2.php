4fe0eeb5f223d38f717a16ba34ea5f78a945b578
Chengzhen Yang <czyang.jessie@gmail.com>
2015-11-04 17:15:25 +0800

    link up issues with system

--- code/application/views/issue/all_2.php
+++ code/application/views/issue/all_2.php
@@ -37,8 +37,8 @@ $this->load->view('common/pm_nav', $class);
 ?>
 <aside class="sidebar-left">
     <div class="sidebar-links">
-        <a class="link-blue" href="projectDashboard.html"><i class="fa fa-tasks"></i>Dashboard</a>
-        <a class="link-blue " href="projectUpdate.html"><i class="fa fa-flag"></i>Update & Milestone</a>
+        <a class="link-blue" href="<?=base_url().'Projects/view_dashboard/'.$project["project_id"]?>"><i class="fa fa-tasks"></i>Dashboard</a>
+        <a class="link-blue " href="<?=base_url().'Projects/view_updates/'.$project["project_id"]?>"><i class="fa fa-flag"></i>Update & Milestone</a>
         <a class="link-blue selected" href="./<?=$repo_slug?>"><i class="fa fa-wrench"></i>Issues</a>
         <a class="link-blue" href="#"><i class="fa fa-folder"></i>File Repository</a>
     </div>
--- code/application/views/project/pm_project_dashboard.php
+++ code/application/views/project/pm_project_dashboard.php
@@ -24,7 +24,7 @@ $this->load->view('common/pm_nav', $class);
     <div class="sidebar-links">
         <a class="link-blue selected" href="<?=base_url().'Projects/view_dashboard/'.$project["project_id"]?>"><i class="fa fa-tasks"></i>Dashboard</a>
         <a class="link-blue " href="<?=base_url().'Projects/view_updates/'.$project["project_id"]?>"><i class="fa fa-flag"></i>Update & Milestone</a>
-        <a class="link-blue " href="projectIssues.html"><i class="fa fa-wrench"></i>Issues</a>
+        <a class="link-blue " href="<?=base_url().'Issues/list_all/'.$project["bitbucket_repo_name"]?>"><i class="fa fa-wrench"></i>Issues</a>
         <a class="link-blue" href="#"><i class="fa fa-folder"></i>File Repository</a>
     </div>
 </aside>
