47a3745f49f883288f0ca858a74f10a74980c0d1
Chengzhen Yang <czyang.jessie@gmail.com>
2016-02-13 17:54:36 +0800

    change esenhower position

--- code/application/views/dashboard/dashboard.php
+++ code/application/views/dashboard/dashboard.php
@@ -143,6 +143,28 @@ $this->load->view('common/pm_nav', $class);
     <div class="col-lg-7">
         <div class="row">
             <div class="col-lg-6">
+                <div class="panel panel-danger">
+                    <div class="panel-heading">Important and Urgent</div>
+                    <div class="panel-body" style="height: 200px;overflow-y: auto;">
+                        <table class="table table-condensed ">
+                            <?php if(isset($tasks_ui)):
+                                foreach($tasks_ui as $t):?>
+                                    <tr>
+                                        <td><a href="<?=base_url().'projects/view_dashboard/'.$t['project_id']?>"><?=$t['content']?></a></td>
+                                        <td> <?php if($t['days']<0){
+                                                $t['days'] = 0-$t['days'];
+                                                echo '<span class="badge" style="background-color: indianred">Overdue'.$t['days'].' days</span>';
+
+                                            }else{
+                                                echo '<span class="badge" style="background-color: darkorange">'.$t['days'].' days</span>';
+                                            }?></td>
+                                    </tr>
+                                <?php endforeach; endif?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
                 <div class="panel panel-warning">
                     <div class="panel-heading">Urgent but not Important</div>
                     <div class="panel-body" style="height: 200px;overflow-y:auto;">
@@ -166,28 +188,6 @@ $this->load->view('common/pm_nav', $class);
                 </div>
             </div>
 
-            <div class="col-lg-6">
-                <div class="panel panel-danger">
-                    <div class="panel-heading">Important and Urgent</div>
-                    <div class="panel-body" style="height: 200px;overflow-y: auto;">
-                        <table class="table table-condensed ">
-                            <?php if(isset($tasks_ui)):
-                                foreach($tasks_ui as $t):?>
-                                    <tr>
-                                        <td><a href="<?=base_url().'projects/view_dashboard/'.$t['project_id']?>"><?=$t['content']?></a></td>
-                                        <td> <?php if($t['days']<0){
-                                                $t['days'] = 0-$t['days'];
-                                                echo '<span class="badge" style="background-color: indianred">Overdue'.$t['days'].' days</span>';
-
-                                            }else{
-                                                echo '<span class="badge" style="background-color: darkorange">'.$t['days'].' days</span>';
-                                            }?></td>
-                                    </tr>
-                                <?php endforeach; endif?>
-                        </table>
-                    </div>
-                </div>
-            </div>
         </div>
         <div class="row">
             <div class="col-lg-6 ">
