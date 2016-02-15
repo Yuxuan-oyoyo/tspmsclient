9f153cdc5dea02376c305052ac99339f1a90aa87
Chengzhen Yang <czyang.jessie@gmail.com>
2016-02-15 23:50:44 +0800

    usecase nav bar fix

--- code/application/views/project/use_case_edit.php
+++ code/application/views/project/use_case_edit.php
@@ -35,7 +35,8 @@ $this->load->view('common/pm_nav', $class);
         <?php
         }
         ?>
-        <a class="link-blue  selected" href="#"><i class="fa fa-list"></i>Use Case List</a>
+        <a class="link-blue" href="<?=base_url().'Usecases/list_all/'.$project["project_id"]?>"><i class="fa fa-list"></i>Use Case List</a>
+        <a class="link-blue" href="<?=base_url().'Projects/view_report/'.$project["project_id"]?>"><i class="fa fa-bar-chart"></i>Analytics</a>
         <a class="link-blue " href="<?=base_url().'upload/upload/'.$project['project_id']?>"><i class="fa fa-folder"></i>File Repository</a>
     </div>
 </aside>
